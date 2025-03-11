370de17a688114c758192118f84b254b352615ef
Timothy-Reagan-rvrvw <timothy.reagan@rvrvw.org>
2025-03-11 18:55:02 +0000

    piplup

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,39 @@
 function submit() {
-    console.log("hbfkhvdhvhdblhbvkhdbKJHS:FHvhjhbidHljhvdLFJhlhjbf");
+    console.log("Hello World");
     var name = document.getElementsByName("name").item(0).value;
     var message = document.getElementsByName("message").item(0).value;
-    console.log(message);
-    alert("hello " + name + " message " + message);
     console.log(name);
+    console.log(message);
+    alert("Hello " + name + "! Your message is: " + message);
     document.getElementById("message").innerHTML = message;
+}
+
+function createNav() {
+    var anchors = [
+        new anchor("Home", "/"),
+        new anchor("My New Page", "/newpage/"),
+        new anchor("More Pages", "/more/"),
+        new anchor("google", "https://google.com"),
+        new anchor("youtube", "https://youtube.com")
+    ];
+    var nav = "";
+    for (var i = 0; i < anchors.length; i++) {
+        if(i != 0) {nav += "|";}
+        nav += anchors[i].toHTML();
+    }
+    document.getElementById("topnav").innerHTML = nav;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    createNav();
+});
+
+class anchor {
+    constructor(name, url) {
+        this.name = name;
+        this.url = url;
+    }
+    toHTML() {
+        return '<a href="' + this.url + '">' + this.name + '</a>';
+    }
 }
